74ba45bb74baebde1429dae03eb425339f9cbf58
Trunova Viktoria <vikit2000@ukr.net>
2022-10-21 00:31:10 +0300

    9 ready

--- js/task-08.js
+++ js/task-08.js
@@ -1,9 +1,30 @@
-const form = document.querySelector('login-form');
+const form = document.querySelector('.login-form');
 
 form.addEventListener('submit', formSubmit);
 
 function formSubmit(event) {
-   event.preventDefault();
-    // const {elements: { email, password }} = event.currentTarget;
-    console.log('eto sabmit formy')
+    event.preventDefault();
+    console.log('eto sabmit formy');
+     
+    const formElem = event.currentTarget.elements;
+    const email = formElem.email.value;
+    const password = formElem.password.value;
+    
+    const formData = {
+        email,
+        password,
+    }
+
+    if (email === '' || password === '') {
+        alert('Please, fill in all the fields!!!')
+    }
+    console.log(formData)
+    event.currentTarget.reset(formData);
 }
+
+// const {elements: { email, password }} = event.currentTarget;
+
+//              ЧТО С ЭТИМ ДЕЛАТЬ
+    // const formDatas = new formData(event.currentTarget);
+// task-08.js:10 Uncaught ReferenceError: Cannot access 'formData' before initialization
+    // at HTMLFormElement.formSubmit
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const button = document.querySelector('.change-color');
+const color = document.querySelector('.color');
+const body = document.querySelector('body');
+
+button.addEventListener('click', changeColor);
+
+function changeColor() {
+  body.style.backgroundColor = getRandomHexColor();
+  color.innerHTML = getRandomHexColor();
+}
+
+
+  
